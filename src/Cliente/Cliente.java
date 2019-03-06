5e6726f4fff604af0e227ef6cdae8966b0f5185c
tapia <tapia641@gmail.com>
2019-03-05 20:10:59 -0600

    Add cliente

---
+++ src/Cliente/Cliente.java
@@ -0,0 +1,23 @@
+package Cliente;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Cliente extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ClienteGUI.fxml"));
+        primaryStage.setTitle("Lado del servidor");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
